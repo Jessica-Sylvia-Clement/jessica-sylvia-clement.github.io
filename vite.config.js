2a3bc797d3000b0d821791faaac2c4419dbd72a0
Jessica Sylvia Clement <jessmotha@gmail.com>
2025-10-12 21:53:46 +1100

    404 file added

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import viteRewriteAll from "vite-plugin-rewrite-all";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/',   // 👈 REQUIRED for GitHub Pages
+  base: "/", // Required for GitHub Pages root domain
+  plugins: [react(), tailwindcss(), viteRewriteAll()],
   build: {
-    target: 'esnext',
-    minify: 'esbuild',
+    target: "esnext",
+    minify: "esbuild",
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.split('node_modules/')[1].split('/')[0];
+          if (id.includes("node_modules")) {
+            return id.split("node_modules/")[1].split("/")[0];
           }
         },
       },
     },
   },
-})
+});
