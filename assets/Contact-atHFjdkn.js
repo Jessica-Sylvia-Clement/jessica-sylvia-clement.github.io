a704e6503f474b46b703884a4314ddc3b92f369c
Jessica Sylvia Clement <jessmotha@gmail.com>
2025-10-12 18:53:36 +1100

    Updates

---
+++ assets/Contact-atHFjdkn.js
@@ -0,0 +1,13 @@
+import{j as e}from"./react--8S78UNW.js";import{d as r,e as t,f as l}from"./react-icons-sozCzmL7.js";import"./cookie-CqkleIqs.js";function o(){return e.jsx("div",{className:"flex flex-col items-center justify-center min-h-screen bg-black text-white px-6",children:e.jsxs("div",{className:"flex flex-col justify-center gap-8 max-w-4xl w-full",children:[e.jsxs("a",{href:"https://www.linkedin.com/in/jessica-sylvia-clement",target:"_blank",rel:"noopener noreferrer",className:`flex flex-col items-center rounded-lg p-6 bg-black \r
+          border border-blue-900/50 hover:border-blue-400/70 \r
+          shadow-[0_0_10px_rgba(0,119,181,0.2)] \r
+          hover:shadow-[0_0_15px_rgba(0,119,181,0.5)]\r
+          transition-all duration-300`,children:[e.jsx(r,{className:"text-4xl text-blue-400 mb-3"}),e.jsx("h3",{className:"text-xl font-semibold mb-2 text-blue-300",children:"LinkedIn"}),e.jsx("p",{className:"text-gray-300 text-center text-sm",children:"linkedin.com/in/jessica-sylvia-clement"})]}),e.jsxs("a",{href:"mailto:jessmotha@gmail.com",className:`flex flex-col items-center rounded-lg p-6 bg-black \r
+          border border-yellow-700/50 hover:border-yellow-400/70\r
+          shadow-[0_0_10px_rgba(255,215,0,0.15)] \r
+          hover:shadow-[0_0_15px_rgba(255,215,0,0.4)]\r
+          transition-all duration-300`,children:[e.jsx(t,{className:"text-4xl text-yellow-300 mb-3"}),e.jsx("h3",{className:"text-xl font-semibold mb-2 text-yellow-200",children:"Email"}),e.jsx("p",{className:"text-gray-300 text-center text-sm",children:"jessmotha@gmail.com"})]}),e.jsxs("a",{href:"https://discord.com/users/763758521174851585",target:"_blank",rel:"noopener noreferrer",className:`flex flex-col items-center rounded-lg p-6 bg-black \r
+          border border-indigo-800/50 hover:border-indigo-400/70\r
+          shadow-[0_0_10px_rgba(114,137,218,0.15)] \r
+          hover:shadow-[0_0_15px_rgba(114,137,218,0.4)]\r
+          transition-all duration-300`,children:[e.jsx(l,{className:"text-4xl text-[#7289da] mb-3"}),e.jsx("h3",{className:"text-xl font-semibold mb-2 text-[#a9b4fa]",children:"Discord"}),e.jsx("p",{className:"text-gray-300 text-center text-sm",children:"@jessicasylviaclement"})]})]})})}export{o as default};
